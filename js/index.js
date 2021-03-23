045f7a9c7d1a14021bb34e14089da18dbf937333
Reid0429 <77335192+Reid0429@users.noreply.github.com>
2021-03-22 21:18:10 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
+let input = document.getElementById('content');
+let add = document.getElementById('add');
+let list = document.getElementById('list');
 
+function addItem() {
+
+    if (input.value != 0) {
+        input.value = input.value.trim();
+
+        //create new list item
+        let li = document.createElement('li');
+        //collect the user input
+
+        li.innerText = input.value;
+        //attach list item to unordered list
+
+        list.appendChild(li);
+
+        document.getElementById('content').value = '';
+
+
+
+    } else {
+        alert("Error");
+    }
+
+}
+add.addEventListener('click', addItem);
